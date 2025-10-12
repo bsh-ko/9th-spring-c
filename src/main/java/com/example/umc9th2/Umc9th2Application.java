87534d72f36929a9793228a3c0f8055b3ec8ebbf
bsh-ko <ssuh3872@gmail.com>
2025-10-12 21:33:15 +0900

    Init: project setup

---
+++ src/main/java/com/example/umc9th2/Umc9th2Application.java
@@ -0,0 +1,13 @@
+package com.example.umc9th2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Umc9th2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Umc9th2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/umc9th2/Umc9th2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.umc9th2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Umc9th2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
